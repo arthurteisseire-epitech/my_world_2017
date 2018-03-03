5d6f664f85589dbb6c44d73f166776a4e65ba203
arthur <arthur.teisseire@epitech.eu>
2018-03-03 05:36:27 +0100

    image added for precision

--- src/init/init_textures.c
+++ src/init/init_textures.c
@@ -16,14 +16,17 @@ int init_textures(world_t *wd)
 	wd->textures[0] = sfTexture_createFromFile("assets/grass.png", NULL);
 	if (wd->textures[0] == NULL)
 		return (-1);
-	wd->textures[1] = sfTexture_createFromFile("assets/grid.png", NULL);
+	wd->textures[1] = sfTexture_createFromFile("assets/precision.png", NULL);
 	if (wd->textures[1] == NULL)
 		return (-1);
-	wd->textures[2] = sfTexture_createFromFile("assets/increase.png", NULL);
+	wd->textures[2] = sfTexture_createFromFile("assets/grid.png", NULL);
 	if (wd->textures[2] == NULL)
 		return (-1);
-	wd->textures[3] = sfTexture_createFromFile("assets/decrease.png", NULL);
+	wd->textures[3] = sfTexture_createFromFile("assets/increase.png", NULL);
 	if (wd->textures[3] == NULL)
+		return (-1);
+	wd->textures[4] = sfTexture_createFromFile("assets/decrease.png", NULL);
+	if (wd->textures[4] == NULL)
 		return (-1);
 	return (0);
 }
--- src/init/init_tools.c
+++ src/init/init_tools.c
@@ -10,10 +10,10 @@
 
 void set_tool_image(world_t *wd)
 {
-	sfRectangleShape_setTexture(wd->toolbar->tool[0].rect, wd->textures[0], sfFalse);
-	sfRectangleShape_setTexture(wd->toolbar->tool[1].rect, wd->textures[1], sfFalse);
-	sfRectangleShape_setTexture(wd->toolbar->tool[2].rect, wd->textures[2], sfFalse);
-	sfRectangleShape_setTexture(wd->toolbar->tool[3].rect, wd->textures[3], sfFalse);
+	sfRectangleShape_setTexture(wd->toolbar->tool[0].rect, wd->textures[1], sfFalse);
+	sfRectangleShape_setTexture(wd->toolbar->tool[1].rect, wd->textures[2], sfFalse);
+	sfRectangleShape_setTexture(wd->toolbar->tool[2].rect, wd->textures[3], sfFalse);
+	sfRectangleShape_setTexture(wd->toolbar->tool[3].rect, wd->textures[4], sfFalse);
 }
 
 void set_tool_call(toolbar_t *toolbar)
